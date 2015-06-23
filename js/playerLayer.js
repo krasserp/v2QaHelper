52248f5ced8ecfa94bbbde3d60bd5fa80d69c8c6
krasserp <krasserp@gmail.com>
2015-06-23 17:12:28 +0100

    adding some more cookies

--- js/playerLayer.js
+++ js/playerLayer.js
@@ -9,6 +9,9 @@ function openPlayer() {
 		$('#player')
 		.prepend(
 			'<div id="controler" style="width:99%;height:20px;  color:#000; padding:5px;"><div id="slider" style="width:40px; background-color:#ff00ff; height:10px; float:left;"></div><span style="float:left; margin-left:100px; color:#202020;">QA tools:</span><span id="switch" style="float:right; color:#fff;" class="ui-icon ui-icon-circle-minus">-</span></div>');
+		var transValue =$.cookie('sliderValue')? parseFloat($.cookie('sliderValue')) : 1;
+
+		$('#playerFrame').css('opacity', transValue);
 		$('#slider').slider({
 			min : 0.2,
 			max : 1,
@@ -17,11 +20,26 @@ function openPlayer() {
 			orientation : "horizontal",
 			slide : function(e, ui) {
 				$('#playerFrame').css('opacity', ui.value)
-
+				$.cookie('sliderValue', ui.value);
 			}
 		});
 	}
-	$('#playerFrame').draggable();
+	$('#playerFrame').draggable({
+			drag: function(){
+	            var offset = $(this).offset();
+	            var xPos = offset.left;
+	            var yPos = offset.top;
+	            console.log('x: ' + xPos, ' y: ' + yPos);
+	        },
+	        stop: function(){
+	            var finalOffset = $(this).offset();
+	            var finalxPos = finalOffset.left;
+	            var finalyPos = finalOffset.top;
+	            console.log('x: ' + finalxPos, ' y: ' + finalyPos);
+	            $.cookie('dragX', finalxPos);
+	            $.cookie('dragY', finalyPos);
+        	}
+        });
 	// setting cookies to determine if the draggable toolbar is collapsed or open
 	if ($.cookie('collapse') == 'hide') {
 		$('.sc-player').removeClass('showme').addClass('collapse')
@@ -67,13 +85,15 @@ function CenterItem(theItem){
 	var windowMiddle=winHeight/2;
 	var itemMiddle=$(theItem).height()/2;
 	var theMiddle=windowMiddle-itemMiddle;
+	var leftXpos = $.cookie('dragX') ? $.cookie('dragX') : theCenter;
+	var topYpos = $.cookie('dragY') ? $.cookie('dragY') : '15';
     if(winWidth>$(theItem).width()){ //horizontal
-    	$(theItem).css('left',theCenter);
+    	$(theItem).css('left',leftXpos+'px');
     } else {
     	$(theItem).css('left','0');
     }
     if(winHeight>$(theItem).height()){ //vertical
-        $(theItem).css('top','15px');  //theMiddle
+        $(theItem).css('top',topYpos+'px');  //theMiddle
     } else {
     	$(theItem).css('top','0');
     }
